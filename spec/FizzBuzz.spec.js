c90ab42bb2d78ebc08da3ff3a7bfe70470582cdb
Joe Grace <joeggrace@gmail.com>
2020-08-08 13:29:22 -0400

    Added jasmine testing
    Returns 1 when given one test working

---
+++ spec/FizzBuzz.spec.js
@@ -0,0 +1,5 @@
+describe("FizzBuzz", () => {
+  it("Returns '1' when given 1", () => {
+    expect(fizzBuzz(1)).toBe("1");
+  });
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,3 @@
+const fizzBuzz = (number) => {
+  return "1";
+};
